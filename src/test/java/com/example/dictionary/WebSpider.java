1f6a42634aafce09e1bd22d44b9be5818d82fc2e
fei386gmail <chenfei386@gmail.com>
2021-08-28 10:49:12 +0800

    webspider with data

--- src/test/java/com/example/dictionary/WebSpider.java
+++ src/test/java/com/example/dictionary/WebSpider.java
@@ -34,55 +34,109 @@ public class WebSpider {
     private CollocationServ collocationServ;
 
     private WebDriver webDriver;
+
     @Test
     public void searchWord()
     {
-        System.setProperty("webdriver.chrome.driver","/Users/chenfei/OneDrive/IDEAProject/proj 5 dictionary/lib/chromedriver");
-        webDriver=new ChromeDriver();
+        //参数
+        String targetWord="amish";
+
+        //开始
+        int countPage=0;
+        boolean breakFlag=false;
+
         PageRequest pageRequest=PageRequest.of(0,100);
         Page<Word> page=wordServ.findAll(pageRequest);
         long totalPages= page.getTotalPages();
-
-        List<Word> words=page.getContent();
-
-        for(int j=0;j<totalPages;j++)
+        System.out.println("totalPages"+totalPages);
+        for (int j=0;j<totalPages;j++)
         {
-            for(int i=0;i<100;i++)
+            System.out.println("Page Number:"+j);
+            PageRequest pageRequest1=PageRequest.of(j,100);
+            Page<Word> page1=wordServ.findAll(pageRequest1);
+            List<Word> words=page1.getContent();
+
+            for (Word word : words) {
+                if (word.getWord().equals(targetWord)) {
+                    countPage = j;
+                    breakFlag = true;
+                    break;
+                }
+            }
+            if (breakFlag) {
+                break;
+            }
+        }
+
+        System.out.println("targetPage:"+countPage);
+    }
+
+    @Test
+    public void searchAndSave()
+    {
+        //参数
+        int startPage=30;
+        int wordsPerPage=100;
+
+        //开始
+        System.setProperty("webdriver.chrome.driver","/Users/chenfei/OneDrive/IDEAProject/proj 5 dictionary/lib/chromedriver");
+        webDriver=new ChromeDriver();
+        PageRequest pageRequest=PageRequest.of(0,wordsPerPage);
+        Page<Word> page=wordServ.findAll(pageRequest);
+        long totalPages= page.getTotalPages();
+
+        for(int j=startPage;j<totalPages;j++)
+        {
+            PageRequest pageRequest1=PageRequest.of(j,wordsPerPage);
+            Page<Word> page1=wordServ.findAll(pageRequest1);
+            List<Word> words=page1.getContent();
+            for(int i=0;i<words.size();i++)
             {
                 Word w=words.get(i);
                 webDriver.get("https://cn.bing.com/dict/search?q="+w.getWord()+"&FORM=HDRSC6");
 
-                saveSynonyms(w.getWord());
-                saveAntonyms(w.getWord());
-                saveCollocations(w.getWord());
+                List<WebElement> h2s= webDriver.findElements(new By.ByXPath("//h2[@class='b_primtxt']"));
+                for (WebElement t: h2s
+                     ) {
+                    switch (t.getText()){
+                        case "同义词": {
+                            t.click();
+                            saveSynonyms(w.getWord());
+                            break;
+                        }
+                        case "反义词": {
+                            t.click();
+                            saveAntonyms(w.getWord());
+                            break;
+                        }
+                        case "搭配":{
+                            t.click();
+                            saveCollocations(w.getWord());
+                            break;
+                        }
+                        default:{
+                            break;
+                        }
+                    }
+                }
+
             }
         }
 
 
-//        webDriver.get("https://cn.bing.com/dict/search?q="+"reverse"+"&FORM=HDRSC6");
-//        saveSynonyms("reverse");
-//        saveAntonyms("reverse");
-//        saveCollocations("reverse");
     }
 
     public void saveSynonyms(String word)
     {
-        try {
-            webDriver.findElement(new By.ByLinkText("同义词")).click();
-        }
-        catch (NoSuchElementException e)
-        {
-            return;
-        }
 
         WebElement synonym=webDriver.findElement(new By.ById("synoid"));
         if(synonym.isDisplayed()){
-            System.out.println("synonym:"+synonym.getLocation());
+//            System.out.println("synonym:"+synonym.getLocation());
         }
 
         List<WebElement> propertiesWebElements=synonym.findElements(new By.ByClassName("df_div2"));
 
-        System.out.println("几种同义词："+propertiesWebElements.size());
+//        System.out.println("几种同义词："+propertiesWebElements.size());
         for (WebElement propertyWebElement: propertiesWebElements
         ) {
             WebElement property=propertyWebElement.findElement(new By.ByClassName("b_dictHighlight"));
@@ -95,13 +149,13 @@ public class WebSpider {
     public   void saveSynonym(String word,WebElement propertyWebElement,String property)
     {
         List<WebElement> spans= propertyWebElement.findElements(new By.ByClassName("b_alink"));
-        System.out.println("几个词："+spans.size());
+//        System.out.println("几个词："+spans.size());
         for (WebElement syn: spans
              ) {
             if(syn.getText().equals(" ") ||syn.getText().equals("") ||syn.getText().equals("   ")){}
             else
             {
-                System.out.println(syn.getText());
+//                System.out.println(syn.getText());
                 Synonym synonym=new Synonym( word,syn.getText(),property);
                 synonymServ.newSynonym(synonym);
              }
@@ -110,22 +164,15 @@ public class WebSpider {
 
     public void saveAntonyms(String word)
     {
-        try {
-            webDriver.findElement(new By.ByLinkText("反义词")).click();
-        }
-        catch (NoSuchElementException e)
-        {
-            return;
-        }
 
         WebElement synonym=webDriver.findElement(new By.ById("antoid"));
         if(synonym.isDisplayed()){
-            System.out.println("antonym:"+synonym.getLocation());
+//            System.out.println("antonym:"+synonym.getLocation());
         }
 
         List<WebElement> propertiesWebElements=synonym.findElements(new By.ByClassName("df_div2"));
 
-        System.out.println("几种反义词："+propertiesWebElements.size());
+//        System.out.println("几种反义词："+propertiesWebElements.size());
         for (WebElement propertyWebElement: propertiesWebElements
         ) {
             WebElement property=propertyWebElement.findElement(new By.ByClassName("b_dictHighlight"));
@@ -136,13 +183,13 @@ public class WebSpider {
     public   void saveAntonym(String word,WebElement propertyWebElement,String property)
     {
         List<WebElement> spans= propertyWebElement.findElements(new By.ByClassName("b_alink"));
-        System.out.println("几个词："+spans.size());
+//        System.out.println("几个词："+spans.size());
         for (WebElement syn: spans
         ) {
             if(syn.getText().equals(" ") ||syn.getText().equals("") ||syn.getText().equals("   ")){}
             else
             {
-                System.out.println(syn.getText());
+//                System.out.println(syn.getText());
                 Antonym antonym=new Antonym(word,syn.getText(),property);
                 antonymServ.newAntonym(antonym);
             }
@@ -151,22 +198,15 @@ public class WebSpider {
 
     public void saveCollocations(String word)
     {
-        try {
-            webDriver.findElement(new By.ByLinkText("搭配")).click();
-        }
-        catch (NoSuchElementException e)
-        {
-            return;
-        }
 
         WebElement synonym=webDriver.findElement(new By.ById("colid"));
         if(synonym.isDisplayed()){
-            System.out.println("collocations:"+synonym.getLocation());
+//            System.out.println("collocations:"+synonym.getLocation());
         }
 
         List<WebElement> propertiesWebElements=synonym.findElements(new By.ByClassName("df_div2"));
 
-        System.out.println("几种搭配："+propertiesWebElements.size());
+//        System.out.println("几种搭配："+propertiesWebElements.size());
         for (WebElement propertyWebElement: propertiesWebElements
         ) {
             WebElement property=propertyWebElement.findElement(new By.ByClassName("b_dictHighlight"));
@@ -177,13 +217,13 @@ public class WebSpider {
     public   void saveCollocation(String word,WebElement propertyWebElement,String property)
     {
         List<WebElement> spans= propertyWebElement.findElements(new By.ByClassName("b_alink"));
-        System.out.println("几个词："+spans.size());
+//        System.out.println("几个词："+spans.size());
         for (WebElement syn: spans
         ) {
             if(syn.getText().equals(" ") ||syn.getText().equals("") ||syn.getText().equals("   ")){}
             else
             {
-                System.out.println(syn.getText());
+//                System.out.println(syn.getText());
                 Collocation collocation=new Collocation(word,syn.getText(),property);
                 collocationServ.newCollocation(collocation);
             }
